9685fae204b6f934a29cccd9885c3fc8115f0a4f
imran-binhasan <imranbinhasan.work@gmail.com>
2024-08-13 10:00:00 +0600

    Update index.html and dir's

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import { RouterProvider } from 'react-router-dom'
+import Router from './routes/Router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>Ready</div>
+    <RouterProvider router={Router}/>
   </StrictMode>,
 )
